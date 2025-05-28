798920a15c521a5b5d64d06411034d0586a66887
ivanoLaVita <russoivan98@gmail.com>
2025-05-28 11:34:50 +0200

    Complete UsersDAO with CRUD operations and validation methods

--- src/main/java/model/UsersDAO.java
+++ src/main/java/model/UsersDAO.java
@@ -4,17 +4,19 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsersDAO extends AbstractDAO<UsersBean> {
 	private static final String TABLE_NAME = "users";
-	
-	
-	//Salva un nuovo utente nel database
+	/*
+	 * Salva un nuovo utente nel database.
+	 */
 	@Override
 	public synchronized void doSave(UsersBean users) throws SQLException {
 		Connection con = null;
 		PreparedStatement ps = null;
-		String query = "INSERT INTO " + UsersDAO.TABLE_NAME + "(id, email, username, password, firstname, lastname, admin) VALUES (?, ?, ?, ?, ?, ?, ?)";
+		String query = "INSERT INTO " + UsersDAO.TABLE_NAME + " (id, email, username, password, firstname, lastname, admin) VALUES (?, ?, ?, ?, ?, ?, ?)";
 		
 		try {
 			con = DriverManagerConnectionPool.getConnection();
@@ -35,17 +37,16 @@ public class UsersDAO extends AbstractDAO<UsersBean> {
 			DriverManagerConnectionPool.releaseConnection(con);
 		}
 	}
-	
 	/*
 	 * Recupera un utente dal database tramite emal.
-	 * Restituisce un UsersBean se trovato, altrimenti null
+	 * Restituisce un UsersBean se trovato, altrimenti null.
 	 */
 	public synchronized UsersBean doRetrieveByEmail(String email) throws SQLException {
 		Connection con = null;
 		PreparedStatement ps = null;
 		UsersBean user = null;
 		
-		String query = "SELECT * FROM " + TABLE_NAME + "Where email = ?";
+		String query = "SELECT * FROM " + TABLE_NAME + " WHERE email = ?";
 		
 		try {
 			con = DriverManagerConnectionPool.getConnection();
@@ -59,8 +60,8 @@ public class UsersDAO extends AbstractDAO<UsersBean> {
 				user.setEmail(rs.getString("email"));
 				user.setEmail(rs.getString("username"));
 				user.setEmail(rs.getString("password"));
-				user.setEmail(rs.getString("nome"));
-				user.setEmail(rs.getString("cognome"));
+				user.setEmail(rs.getString("firstname"));
+				user.setEmail(rs.getString("lastname"));
 			}
 		} finally {
 			if (ps != null) ps.close();
@@ -68,17 +69,48 @@ public class UsersDAO extends AbstractDAO<UsersBean> {
 		}
 		return user;
 	}
-	
 	/*
-	 * Controlla se la mail è gia stata registrata nel database
-	 * Restituisce true se trovata, false altrimenti
+	 * Recupera un utente dal database utilizzando L'ID come chiave.
+	 */
+	public synchronized UsersBean doRetrieveById(int id) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		UsersBean user = null;
+		
+		String query = "SELECT * FROM " + TABLE_NAME + " WHERE id = ?";
+		
+		try {
+			con = DriverManagerConnectionPool.getConnection();
+			ps = con.prepareStatement(query);
+			ps.setInt(1,id);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				user = new UsersBean();
+		            user.setEmail(rs.getString("email"));
+		            user.setUsersname(rs.getString("username"));
+		            user.setPassword(rs.getString("password"));
+		            user.setFirstName(rs.getString("firtsname"));
+		            user.setLastName(rs.getString("lastname"));
+		            user.setAdmin(rs.getBoolean("admin"));
+			}
+		} finally {
+			if (ps != null) ps.close();
+			DriverManagerConnectionPool.releaseConnection(con);
+		}
+		return user;
+	}
+	/*
+	 * Controlla se la mail è gia stata registrata nel database.
+	 * Restituisce true se trovata, false altrimenti.
 	 */
 	public synchronized boolean checkEmail(String email) throws SQLException {
 		Connection con = null;
 		PreparedStatement ps = null;
 		boolean found = false;
 		
-		String query = "SELECT 1 FROM " + TABLE_NAME + "WHERE email = ? LIMIT 1";
+		String query = "SELECT 1 FROM " + TABLE_NAME + " WHERE email = ? LIMIT 1";
 		
 		try {
 			con = DriverManagerConnectionPool.getConnection();
@@ -93,17 +125,16 @@ public class UsersDAO extends AbstractDAO<UsersBean> {
 		}
 		return found;
 	}
-	
 	/*
-	 * Controlla se un username è gia' in uso
-	 * Restituisce true se giaì in uso
+	 * Controlla se un username è gia' in uso.
+	 * Restituisce true se giaì in uso.
 	 */
 	public synchronized boolean checkUsername(String username) throws SQLException {
 		Connection con = null;
 		PreparedStatement ps = null;
 		boolean found = false;
 		
-		String query = "SELECT 1 FROM " + TABLE_NAME + "WHERE username = ? LIMIT 1";
+		String query = "SELECT 1 FROM " + TABLE_NAME + " WHERE username = ? LIMIT 1";
 		
 		try {
 			con = DriverManagerConnectionPool.getConnection();
@@ -117,5 +148,93 @@ public class UsersDAO extends AbstractDAO<UsersBean> {
 			DriverManagerConnectionPool.releaseConnection(con);
 		}
 		return found;
+	}
+	/*
+	 * Aggiorna le informazioni di un utente nel database
+	 */
+	public synchronized boolean doUpdate(UsersBean user) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		int result = 0;
+		
+		String query = "UPDATE users SET email = ?, username = ?, password = ?, firstname = ?, lastname = ?, admin = ? WHERE id = ?";
+		
+		try {
+			con = DriverManagerConnectionPool.getConnection();
+			ps = con.prepareStatement(query);
+			
+
+	        ps.setString(1, user.getEmail());
+	        ps.setString(2, user.getUsersname());
+	        ps.setString(3, user.getPassword());
+	        ps.setString(4, user.getFirstName());
+	        ps.setString(5, user.getLastName());
+	        ps.setBoolean(6, user.isAdmin());
+	        ps.setInt(7, user.getId());
+	        
+	        ps.executeUpdate();
+	        con.commit();
+		} finally {
+			if (ps != null) ps.close();
+			DriverManagerConnectionPool.releaseConnection(con);
+		}
+		return result != 0; //true se l update ha modificato almeno una riga.
+	}
+	/*
+	 * Elimina un utente dal database usando l'ID come chiave
+	 */
+	public synchronized void doDelete(int id) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		
+		String query = "DELETE FROM users WHERE id = ?";
+		
+		try {
+			con = DriverManagerConnectionPool.getConnection();
+			ps = con.prepareStatement(query);
+			ps.setInt(1, id);
+			
+			ps.executeQuery();
+			con.commit();
+		} finally {
+			if (ps != null) ps.close();
+			DriverManagerConnectionPool.releaseConnection(con);
+		}
+	}
+	/*
+	 * Recupera tutti gli utenti dal database con la possibilità di specificare un ordine
+	 */
+	public synchronized List<UsersBean> doRetrieveAll(String order) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		List<UsersBean> users = new ArrayList<>();
+		
+		String query = "SELECT * FROM users ";
+		if (order != null && !order.isEmpty()) {
+			query += "ORDER BY " + order;
+		}
+		
+		try {
+			con = DriverManagerConnectionPool.getConnection();
+			ps = con.prepareStatement(query);
+			
+			ResultSet rs = ps.executeQuery();
+			
+	        while (rs.next()) {
+	            UsersBean user = new UsersBean();
+	            user.setId(rs.getInt("id"));
+	            user.setEmail(rs.getString("email"));
+	            user.setUsersname(rs.getString("username"));
+	            user.setPassword(rs.getString("password"));
+	            user.setFirstName(rs.getString("firstName"));
+	            user.setLastName(rs.getString("lastName"));
+	            user.setAdmin(rs.getBoolean("admin"));
+	            users.add(user);
+	        }
+		} finally {
+			if (ps != null) ps.close();
+			DriverManagerConnectionPool.releaseConnection(con);
+		}
+		return users;
 	}
 }
