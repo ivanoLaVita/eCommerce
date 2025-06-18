5547ac9ed6928a8fd87a30bed8fd68522527bbf0
ivanoLaVita <russoivan98@gmail.com>
2025-06-18 14:54:32 +0200

    Update LogoutServlet and EditInfoServlet--Refactor UsersBean,UsersDAO

---
+++ src/main/java/controller/LogoutServlet.java
@@ -0,0 +1,42 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LogoutServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Rimuove gli attributi di sessione
+        request.getSession().removeAttribute("firstName");
+        request.getSession().removeAttribute("lastName");
+        request.getSession().removeAttribute("logged");
+        request.getSession().removeAttribute("admin");
+        request.getSession().removeAttribute("orders");
+        request.getSession().removeAttribute("paymentMethods");
+        request.getSession().removeAttribute("addresses");
+        request.getSession().removeAttribute("cart");
+
+        // Invalida la sessione
+        request.getSession().invalidate();
+
+        // Reindirizza alla pagina di login
+        response.sendRedirect("loginPage.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
